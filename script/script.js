cdc32992a47ed541c506ecd98de4ba459905845a
ameera <ameeraabedalnaser2021@gmail.com>
2021-07-14 11:59:22 +0300

    create remove btn product

--- script/script.js
+++ script/script.js
@@ -5,12 +5,13 @@ const btnBuyer = document.querySelector(".switch-buyer");
 const headerSection = document.querySelector(".header");
 const inputSearch = document.querySelector(".input-search");
 const btnAdd = document.querySelector(".btn-add-product");
+const productForm = document.getElementById("product-form");
 
 
+let id = 1;
 
 btnSeller.addEventListener("click", moveToSeller);
 btnBuyer.addEventListener("click", moveToBuyer);
-
 
 // displays the filtered(searched) items and removes the non-searched ones
 const updateResult = () => {
@@ -28,48 +29,48 @@ const updateResult = () => {
 
 }
 
-
-
 inputSearch.addEventListener('input', updateResult);
 
 function moveToBuyer() {
     seller.style.display = "none";
     buyer.style.display = "flex";
-    switchBtnAddProduct();
+    hideBtnAddProduct();
+
 }
 
 function moveToSeller() {
     buyer.style.display = "none";
     seller.style.display = "flex";
-    switchBtnAddProduct();
+    showBtnAddProduct();
+    buildSeller();
+    btnSeller.disabled = true;
+
 }
 
 
 const arr = [{
         name: 'book7',
-        imgSrc: "https://via.placeholder.com/150",
-        price: "25$",
-    },
-    {
-        name: 'book1',
-        imgSrc: "https://via.placeholder.com/150",
-        price: "40$"
-    }, {
-        name: 'book2',
-        imgSrc: "https://via.placeholder.com/150",
-        price: "40$"
-    },
-    {
-        name: 'book3',
-        imgSrc: "https://via.placeholder.com/150",
-        price: "40$"
+        url: "https://static.libertyprim.com/files/familles/fraise-large.jpg?1569271765",
+        price: 25,
+        category: "fruits",
+        id: 0,
     },
     {
         name: 'book4',
-        imgSrc: "https://via.placeholder.com/150",
-        price: "40$"
+        url: "https://static.libertyprim.com/files/familles/fraise-large.jpg?1569271765",
+        price: 25,
+        category: "fruits",
+        id: 0,
+    }, {
+        name: 'book5',
+        url: "https://static.libertyprim.com/files/familles/fraise-large.jpg?1569271765",
+        price: 25,
+        category: "fruits",
+        id: 0,
     }
 ];
+
+
 
 
 function buildBuyer(arr) {
@@ -92,7 +93,7 @@ function buildBuyer(arr) {
         ele.appendChild(name);
         ele.appendChild(pricePara);
 
-        img.src = arr[i].imgSrc;
+        img.src = arr[i].url;
         name.textContent = arr[i].name;
         pricePara.textContent = arr[i].price;
         let btnAddToCard = document.createElement("button");
@@ -106,7 +107,7 @@ buildBuyer(arr);
 
 
 function buildSeller() {
-
+    console.log(arr);
     for (let i = 0; i < arr.length; i++) {
         let ele = document.createElement("div")
         ele.classList.add("item");
@@ -121,26 +122,75 @@ function buildSeller() {
         ele.appendChild(img);
         ele.appendChild(pricePara);
 
-        img.src = arr[i].imgSrc;
+        img.src = arr[i].url;
         pricePara.textContent = arr[i].price;
 
         let btnRemoveItem = document.createElement("button");
         btnRemoveItem.classList.add("btn-to-removeItem");
+        btnRemoveItem.setAttribute("id", arr[i].name);
+        btnRemoveItem.setAttribute("onclick", "remove(event);");
         btnRemoveItem.textContent = "Remove the Product";
         ele.appendChild(btnRemoveItem);
+
+
     }
 
 }
-buildSeller();
 
-function switchBtnAddProduct() {
 
-    btnAdd.classList.toggle("hide");
+function showBtnAddProduct() {
+    btnAdd.classList.remove("hide");
+}
+
+function hideBtnAddProduct() {
+    btnAdd.classList.add("hide");
 }
 
 btnAdd.addEventListener("click", displayForm);
 
 function displayForm() {
-    const productForm = document.getElementById("product-form");
     productForm.classList.remove("hide");
+}
+
+// function for submit button
+
+const submitBtn = document.querySelector(".btn-add");
+submitBtn.addEventListener("click", saveInputValue)
+
+function saveInputValue() {
+    const arrOfInputs = document.querySelectorAll(".input");
+    let objectOfNewProduct = {}
+
+    objectOfNewProduct["name"] = arrOfInputs[0].value
+    objectOfNewProduct["url"] = arrOfInputs[1].value;
+    objectOfNewProduct["price"] = arrOfInputs[2].value;
+    objectOfNewProduct["category"] = arrOfInputs[3].value;
+
+    // let indexOfItem =
+    arr.push(objectOfNewProduct);
+    // console.log(indexOfItem)
+    editSection();
+    id = id + 1;
+
+}
+
+function editSection() {
+
+    buyer.textContent = null;
+
+    buildBuyer(arr);
+    buildSeller();
+}
+//  remove product
+function remove(event) {
+    let s = event.target;
+    console.log(s.id);
+    for (let i = 0; i < arr.length; i++) {
+        if (s.id == arr[i].name) {
+            arr.splice(i, 1);
+        }
+    }
+    let element = document.getElementById(s.id);
+    element.parentNode.remove();
+
 }
